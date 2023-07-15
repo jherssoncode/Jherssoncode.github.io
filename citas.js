ca2613e9ea5766b981c2f039733f10bf956c3ad4
jherssoncode <108030530+jherssoncode@users.noreply.github.com>
2023-07-14 21:13:19 -0500

    Subida de pagina

---
+++ citas.js
@@ -0,0 +1,42 @@
+let citas = [
+  {
+    'autor': 'Te amo Karen',
+    'texto': 'Eres la luz que ilumina mi vida y el amor que llena mi corazón. Te amo, mi amor.'
+  },
+  {
+    'autor': 'Gracias mi amor',
+    'texto': 'Cada día a tu lado es un regalo. Gracias por amarme y hacerme sentir completo. Te adoro, mi vida.'
+  },
+  {
+    'autor': 'Eres mi vida Karen',
+    'texto': 'Tu amor es mi refugio, mi paz y mi fortaleza. Te amo más de lo que las palabras pueden expresar.'
+  },
+  {
+    'autor': 'Mi motivación',
+    'texto': 'Eres mi razón de ser, mi inspiración y mi motivación. Gracias por amarme incondicionalmente. Eres todo para mí, Karen.'
+  },
+  {
+    'autor': 'Mi dulce amor',
+    'texto': 'En tus ojos encuentro mi hogar, en tus abrazos encuentro mi paz. No hay lugar en el mundo donde prefiera estar que a tu lado. Te amo, mi dulce amor.'
+  },
+  {
+    'autor': 'Mi amor por siempre',
+    'texto': 'Eres la melodía que llena mi corazón de alegría, el sueño que nunca quiero despertar. Karen eres mi amor eterno.'
+  },
+  {
+    'autor': 'Eres mi medicina',
+    'texto': 'Tus besos son el elixir que cura todas mis heridas. Tu amor es mi medicina. Gracias por amarme. Eres mi mundo, mi tesoro.'
+  },
+  {
+    'autor': 'Por siempre a tu lado',
+    'texto': 'Cada momento a tu lado es un regalo precioso. No puedo esperar a pasar el resto de mi vida contigo. Te amo infinitamente, mi amor.'
+  },
+  {
+    'autor': 'Te amo con todo mi ser',
+    'texto': 'Tu sonrisa ilumina mi día y tu amor llena mi vida de felicidad. Gracias por ser la persona más maravillosa que he conocido. Te amo con todo mi ser.'
+  },
+  {
+    'autor': 'Eres mi todo',
+    'texto': 'Eres mi adoración, mi verdadero amor. No puedo imaginar una vida sin ti.'
+  }
+];
